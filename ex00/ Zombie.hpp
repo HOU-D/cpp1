2974067fe9c905d627f75ba12601bc7bec9815c3
Houda Akoumi <hoakoumi@e1r13p12.1337.ma>
2024-01-13 23:15:57 +0100

    h

---
+++ ex00/ Zombie.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*    Zombie.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoakoumi <hoakoumi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/13 21:40:33 by hoakoumi          #+#    #+#             */
+/*   Updated: 2024/01/13 21:45:24 by hoakoumi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef Zombie
+# define Zombie
+
+#include <iostream>
+
+class Zombie
+{
+    private :
+    
+        std :: string name;
+
+    public :
+
+        void announce( void ); 
+}
+
+
+
+#endif
